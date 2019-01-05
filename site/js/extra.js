0d5eff8a18ff458da0af1343f0e5b35410835904
keuch <master@identitaetsmanagement.eu>
2019-01-05 02:24:25 +0100

    leninisiert

---
+++ site/js/extra.js
@@ -0,0 +1,10 @@
+var dgby=function( id ) { return document.getElementById( id ); };
+
+function iswp(image) {
+	console.log(image.src);
+	image.setAttribute('src', '/images/me2.png');
+}
+function iswp2(image) {
+	console.log(image.src);
+	image.setAttribute('src', '/images/me1.png');
+}
