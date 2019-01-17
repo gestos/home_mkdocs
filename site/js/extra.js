0eaf0d776d8880a4d69450b0f2237d91b4ab7c91
keuch <master@identitaetsmanagement.eu>
2019-01-17 03:15:42 +0100

    randpix

--- site/js/extra.js
+++ site/js/extra.js
@@ -1,10 +1,12 @@
 var dgby=function( id ) { return document.getElementById( id ); };
 
+var rndimg = ['/images/me2.png','/images/me3.png','/images/me4.png','/images/me5.png','/images/me6.png','/images/me7.png','/images/me8.png','/images/me9.png','/images/me10.png']
 function iswp(image) {
-	console.log(image.src);
-	image.setAttribute('src', '/images/me2.png');
+	var rndnum=Math.floor(Math.random() * rndimg.length);
+	console.log(rndimg[rndnum]);
+	image.setAttribute('src', rndimg[rndnum]);
 }
 function iswp2(image) {
 	console.log(image.src);
-	image.setAttribute('src', '/images/me1.png');
+	image.setAttribute('src', '/images/me.png');
 }
